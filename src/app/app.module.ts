ba003e51b465ab7fdba16de102c70ec74e1cd365
Roman Lagonsky <roman.workmail4@gmail.com>
2022-12-27 10:49:06 +0100

    US 001: Created Angular Project & file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { LoginPageComponent } from '@/user/login-page/login-page.component';
+import { MainLayoutComponent } from '@/shared/components/main-layout/main-layout.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
